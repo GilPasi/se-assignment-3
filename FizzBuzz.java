c0072ace947e40daa0d7c9a161f73539b9f5017b
Morin Ben Harush <103103157+morinlugasi@users.noreply.github.com>
2024-02-21 15:45:06 +0200

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -7,10 +7,15 @@ public class FizzBuzz {
 	public String calculateFizzBuzz(int i) {
 		
 		for (int j=1; j<=i; j++) {
-			if(j == 3)
+			if (j %3 ==0 && j%5 ==0)
+				str.append("FizzBuzz");
+			
+			else if(j % 3 ==0)
 				str.append("Fizz");
-			else if ( j == 5)
+			
+			else if ( j % 5==0)
 				str.append("Buzz");
+			
 			else
 				str.append(j);
 			
